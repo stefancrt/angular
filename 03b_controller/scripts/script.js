1652403d9d0ffc0b913b411a70ce560184d8b95e
Stefan <stefancrt22@yahoo.com>
2017-05-30 19:54:08 +0200

    sss

--- 03b_controller/scripts/script.js
+++ 03b_controller/scripts/script.js
@@ -1,14 +1,28 @@
-// Ejemplo del controller utilizando inyecciÃ³n del $scope
-// como era mÃ¡s habitual en los inicios de AngularJS
+// Ejemplo del controller NO utilizando inyecciÃ³n del $scope,
+// sino la forma controller ... as en la directiva
+// junto con la definiciÃ³n de clase propia de ES6/2015
+// tal como recomienta la guÃ­a de estilo de AngularJS (ES2015) de Todd Motto
 
-angular.module("miApp", [])
-.controller("MainController", ["$scope", function ($scope) {
-    $scope.nombre = 'Pepe'; 
-    $scope.nuevoPensamiento = ''; // declaracion innecesaria pero recomendable
-    $scope.aPensamientos = [];
 
-    $scope.botonAdd = function () {
-        $scope.aPensamientos.push($scope.nuevoPensamiento); 
-        $scope.nuevoPensamiento = '';
+ class AcumuladorCtrl {
+
+    //constructor () {
+    //}
+
+    $onInit () {
+       this.number = 10;
+			this.total = this.number;
     }
-}]); // fin de Maincontroller
+
+   botonAdd () {
+				this.total += parseInt(this.number);
+			}
+       
+    botonRest () {
+        this.total -= parseInt(this.number);
+    }   
+} // Fin de la clase
+
+
+angular.module("acumulador", [])
+.controller("AcumuladorCtrl", AcumuladorCtrl );
